b99b1ec1ad78d1017da4b85e16b9c8515a2514ba
jomamma417 <johnnyl@cpp.edu>
2019-10-22 19:55:46 -0700

    Initialize repo

---
+++ newtInterp.java
@@ -0,0 +1,92 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class newtInterp {
+
+	public static void main(String[] args) {
+		String fileName = args[0];
+		read(fileName);
+
+	}
+	static void read(String fileName) {
+		try {
+			
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/" + fileName));
+			
+			Scanner kb = new Scanner(System.in);
+			String[] inputString; 
+
+			//Read in x vector
+			String line = bufferedReader.readLine();
+			inputString = line.split("\\s+");
+			float[] xVector = new float[inputString.length];
+			for(int i = 0; i < xVector.length; i++) {
+				xVector[i] = Float.parseFloat(inputString[i]);
+			}
+			System.out.println("X VECTOR: " + Arrays.toString(xVector));
+			
+			//Read in y vector
+			line = bufferedReader.readLine();
+			inputString = line.split("\\s+");
+			float[] yVector = new float[inputString.length];
+			for(int i = 0; i < yVector.length; i++){
+				yVector[i] = Float.parseFloat(inputString[i]);
+			}
+			System.out.println("Y VECTOR: " + Arrays.toString(yVector));
+			
+			String input;
+			System.out.println("Enter 'q' to quit");
+				do {
+
+					input = kb.nextLine();
+					Coeff(xVector, yVector, new float[xVector.length], Float.parseFloat(input));	
+					
+				}while(!input.equalsIgnoreCase("q"));
+
+				
+			bufferedReader.close();
+		}//end try
+		catch(FileNotFoundException e) {
+		}
+		catch(IOException e) {
+		}//end catch
+	}//end read
+
+	static void Coeff(float[] xs, float[] ys, float[] cs, float z){
+		int n = xs.length-1;
+
+		for(int i = 0; i <= n; i++){
+			cs[i] = ys[i];
+		}
+		System.out.println("1: " + Arrays.toString(cs));
+		for(int j = 1; j <= n; j++){
+			for(int i = n; i >= j; i--){
+				cs[i] = (cs[i] - cs[i-1]) / (xs[i] - xs[i-j]);
+			} //end for
+		} //end for
+		System.out.println("2: " + Arrays.toString(cs));
+
+		EvalNewton(xs, cs, z);
+	} //end Coeff
+	
+	static void EvalNewton(float[] xs, float[] cs, float z){
+		int n = xs.length-1;
+		float result = cs[n];
+
+		for(int i = (n-1); i >= 0; i--){
+			result = result * (z - xs[i]) + cs[i];
+		} //end for
+
+		System.out.println(result);
+
+	} //end EvalNewton
+	
+
+}
